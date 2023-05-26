51ed4f17e9de1e5d6ad3e0793c1be7807103457d
Seraphim Strub <sst@relay.rievo.net>
2023-05-26 14:18:05 +0200

    remove unneeded options

--- post-install.sh
+++ post-install.sh
@@ -5,15 +5,11 @@ set -ouex pipefail
 systemctl enable rpm-ostreed-automatic.timer
 systemctl enable flatpak-system-update.timer
 
-# set up PAM for systemd-homed
-authselect enable-feature with-systemd-homed
-
 # enable tty1
 systemctl enable getty@tty1.service
 
 systemctl enable systemd-timesyncd.service
 systemctl enable systemd-resolved.service
-systemctl enable systemd-homed.service
 
 systemctl --global enable flatpak-user-update.timer
 
