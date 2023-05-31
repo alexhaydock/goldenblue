2fb4c097ba3a274d032c6c9e29cda4bc27335ed4
Seraphim Strub <sst@relay.rievo.net>
2023-05-31 13:24:54 +0200

    adds packages and env

---
+++ etc/profile.d/xdg-user-dirs.sh
@@ -0,0 +1,11 @@
+XDG_DATA_HOME="$HOME/.local/share"
+XDG_CONFIG_HOME="$HOME/.config"
+XDG_STATE_HOME="$HOME/.local/state"
+XDG_CACHE_HOME="$HOME/.cache"
+
+export XDG_DATA_HOME XDG_CONFIG_HOME XDG_STATE_HOME XDG_CACHE_HOME
+
+export GOPATH="$XDG_DATA_HOME"/go
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
+
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
