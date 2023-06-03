21e5e125cf3717d46849e76d1362a43d097de0a1
Seraphim Strub <sst@relay.rievo.net>
2023-06-03 21:06:32 +0200

    move .java back to default location

--- etc/profile.d/xdg-user-dirs.sh
+++ etc/profile.d/xdg-user-dirs.sh
@@ -6,7 +6,6 @@ XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME XDG_CONFIG_HOME XDG_STATE_HOME XDG_CACHE_HOME
 
 export GOPATH="$XDG_DATA_HOME"/go
-export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME"/java
 
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
 
