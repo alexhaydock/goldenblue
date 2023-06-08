e5faf1b55b7540dab8ab8407679a86a33164c054
Alex Haydock <alex@alexhaydock.co.uk>
2023-06-08 21:52:03 +0100

    More updates to make this specific to my own deployment

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Basically Fedora Silverblue, except my own golden image version of it... get it?
 
 With thanks to [Seraphim Strub](https://dev.rievo.net/sst/uphy) for the original GitLab CI friendly version of this code, which is in turn based on much of the [uBlue](https://ublue.it/) project's work.
+
+### Notes
+#### /etc/vconsole.conf
+Sets the console font to `latarcyrheb-sun32` which is a large size font. Seems to be a good choice for high DPI displays so I left this intact.
--- post-install.sh
+++ post-install.sh
@@ -2,32 +2,33 @@
 
 set -ouex pipefail
 
-pip install --prefix=/usr yafti
+# (Disabled) install yafti via Pip
+#pip install --prefix=/usr yafti
 
+# Enable rpm-ostree automatic updates
+cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
 systemctl enable rpm-ostreed-automatic.timer
+
+# Enable the Flatpak auto-updater we inherit from uBlue
+# (Allows us to remove GNOME Software)
 systemctl enable flatpak-system-update.timer
+systemctl --global enable flatpak-user-update.timer
 
 # enable tty1
 systemctl enable getty@tty1.service
 
-systemctl enable systemd-timesyncd.service
-systemctl enable systemd-resolved.service
-
-systemctl --global enable flatpak-user-update.timer
-
-cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
+#systemctl enable systemd-timesyncd.service
+#systemctl enable systemd-resolved.service
 
 # move OS systemd unit defaults to /usr
-cp -a --verbose /etc/systemd/system /etc/systemd/user /usr/lib/systemd/
-rm -r /etc/systemd/system /etc/systemd/user
+# (commented out because I'm not sure why upstream does this)
+#cp -a --verbose /etc/systemd/system /etc/systemd/user /usr/lib/systemd/
+#rm -r /etc/systemd/system /etc/systemd/user
 
 # try to remove dmenu
-rpm-ostree override remove dmenu
+#rpm-ostree override remove dmenu
 
-ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
+# Configure Europe/London as our timezone
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 
-authselect enable-feature with-fingerprint
-
-# DesktopEntry Execution: https://github.com/jceb/dex
-curl -o /usr/bin/dex https://raw.githubusercontent.com/jceb/dex/942fe998db240357dc839e1df07b4fe6668a1c22/dex
-chmod +x /usr/bin/dex
+# authselect enable-feature with-fingerprint
